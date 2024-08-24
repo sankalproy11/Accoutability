3f423104a75812dd9404d9d7593b65cc187d32f3
sankalproy11 <sankalproy.dev@gmail.com>
2024-08-24 17:23:44 +0530

    added config.js for the connecting db

--- backend/src/db/config.js
+++ backend/src/db/config.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGO_URI}/${process.env.DB_NAME}`
     );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
@@ -14,4 +14,4 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+module.exports = connectDB;
